cab2fc7ba94910748bec5c314cb5e8264cd9508b
Kevin Python <kevinlee577@yahoo.com>
2019-12-16 23:54:20 -0600

    File updates

---
+++ Data/RazorPagesMovieNewContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovieNew.Models;
+
+namespace RazorPagesMovieNew.Data
+{
+    public class RazorPagesMovieNewContext : DbContext
+    {
+        public RazorPagesMovieNewContext (DbContextOptions<RazorPagesMovieNewContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovieNew.Models.Movie> Movie { get; set; }
+    }
+}
